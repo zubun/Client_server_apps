ac03248fac50f33940d90c672bf85b85f22140ab
zubun <zubun7@gmail.com>
2022-05-18 21:45:21 +0300

    Initial commit

---
+++ hw_1_5.py
@@ -0,0 +1,20 @@
+'''5. Написать код, который выполняет пинг веб-ресурсов yandex.ru, youtube.com и преобразовывает результат из
+байтовового типа данных в строковый без ошибок для любой кодировки операционной системы.'''
+
+import platform
+import subprocess
+from chardet import detect
+
+site_urls = ['yandex.ru', 'youtube.com']
+code = '-n' if platform.system().lower() == 'windows' else '-c'
+
+
+for url in site_urls:
+    args = ['ping', code, '4', url]
+    ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+    for line in ping.stdout:
+        result = detect(line)
+        print(result)
+        line = line.decode(result['encoding']).encode('utf-8')
+        print(line.decode('utf-8'))
+
---
+++ hw_1_6.py
@@ -0,0 +1,42 @@
+'''6. Создать текстовый файл test_file.txt, заполнить его тремя строками: «сетевое программирование», «сокет»,
+«декоратор». Далее забыть о том, что мы сами только что создали этот файл и исходить из того, что перед нами файл в
+неизвестной кодировке. Задача: открыть этот файл БЕЗ ОШИБОК вне зависимости от того, в какой кодировке он был создан.'''
+import random
+
+from chardet import detect
+
+encoding = ['"cp866"', '"utf8"', '"cp1251"', '"UTF-16"']
+
+
+file = open('test_file.txt', 'w', encoding=random.choice(encoding))
+file.write('сетевое программирование, сокет, декоратор')
+file.close()
+
+with open('test_file.txt', 'rb') as f:
+    content = f.read()
+encoding = detect(content)['encoding']
+print('encoding: ', encoding)
+
+with open('test_file.txt', encoding=encoding) as f_n:
+    for el_str in f_n:
+        print(el_str, end='\n' + '-' * 10)
+    print()
+
+# # --- 1. создаём файл в заданной кодировке
+# f = open('test.txt', 'w', encoding='utf-8')
+# f.write('тест тест тест')
+# f.close()
+#
+#
+# # --- 2. узнаём кодировку (как будто бы!) неизвестного нам файла
+# with open('test.txt', 'rb') as f:
+#     content = f.read()
+# encoding = detect(content)['encoding']
+# print('encoding: ', encoding)
+#
+#
+# # --- 3. Теперь открываем файл в УЖЕ известной нам кодировке
+# with open('test.txt', encoding=encoding) as f_n:
+#     for el_str in f_n:
+#         print(el_str, end='')
+#     print()
